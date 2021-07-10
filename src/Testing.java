945f029e46168d56e7a861194eb04c006347e213
Evan Cooksley <evanowain@shaw.ca>
2021-07-10 10:02:48 -0600

    Got info from api and saved as string

--- src/Testing.java
+++ src/Testing.java
@@ -1,8 +1,36 @@
-
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
 public class Testing {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			URL url = new URL("https://api.coindesk.com/v1/bpi/currentprice.json");
+		
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.connect();
+		
+			int responsecode = conn.getResponseCode();
+			
+			if (responsecode != 200) {
+				throw new RuntimeException("HttpResponseCode: " + responsecode);
+			} else {
+				String inline = "";
+				Scanner sc = new Scanner(url.openStream());
+				
+				while(sc.hasNext()) {
+					inline += sc.nextLine();
+				}
+				
+				sc.close();
+				
+				System.out.println(inline);
+			}
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
